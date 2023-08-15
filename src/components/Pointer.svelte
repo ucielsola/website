fcc9e19ece3bc097826cacfa2878e7931c0ef1d3
Uciel Sola <solauciel@gmail.com>
2023-08-15 14:55:23 -0300

    fix scrolling issue

--- src/components/Pointer.svelte
+++ src/components/Pointer.svelte
@@ -6,9 +6,7 @@
 		$position.y - correction
 	}px;`;
 
-	const correction = 32;
-	let scrollY = 0;
-	let scrollX = 0;
+	const correction = 30
 
 	const position = tweened(
 		{ x: 0, y: 0 },
@@ -20,15 +18,16 @@
 
 	const handleMouseMove = (e: MouseEvent) => {
 		position.set({
-			x: e.clientX + scrollX,
-			y: e.clientY + scrollY,
+			x: e.clientX,
+			y: e.clientY,
 		});
 	};
 </script>
 
-<svelte:window bind:scrollY bind:scrollX on:mousemove={handleMouseMove} />
+<svelte:window on:mousemove={handleMouseMove} />
 
 <div
-	class="absolute w-16 h-16 rounded-full bg-dracula-400/75 blur-3xl pointer-events-none"
+	class="sticky w-16 h-16 rounded-full bg-dracula-400/75 blur-3xl pointer-events-none opacity-0 transition-opacity duration-700"
+	class:opacity-100={$position.x && $position.y}
 	style={circlePosition}
 />
