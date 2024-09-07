f3094af6cb9a6ed83b433eb358ac430d284f01a9
Uciel Sola <solauciel@gmail.com>
2024-09-07 20:45:28 -0300

    ref: vercel analytics alternate approach

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
@@ -10,5 +10,9 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), svelte()]
+  integrations: [icon(), tailwind(), svelte()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
