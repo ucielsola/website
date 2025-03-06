ffc14c8e8eca46359f8cd0faf2c47c19fbcd0970
Uciel Sola <solauciel@gmail.com>
2025-03-06 15:53:13 -0300

    add header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,37 @@
 <script lang="ts">
+	import GithubIcon from '$lib/components/GithubIcon.svelte';
 	import Input from '$lib/components/Input.svelte';
+	import LinkedInIcon from '$lib/components/LinkedInIcon.svelte';
 	import MessageList from '$lib/components/MessageList.svelte';
 </script>
 
-<div class="relative flex h-screen w-screen flex-col overflow-hidden pt-4">
-	<h1 class="absolute top-0 text-white/0">Uciel Sola</h1>
+<svelte:head>
+    <title>Uciel Sola</title>
+</svelte:head>
+
+<div class="relative flex h-screen w-screen flex-col gap-4 overflow-hidden pt-4">
+	<div class="flex items-center justify-between px-6">
+		<h1 class="absolute top-0 text-white/0">Uciel Sola</h1>
+		<h2 class="text-xl">👋 Hi there!</h2>
+		<div class="flex items-center gap-3">
+			<a
+				class="text-accent hover:text-primary h-10 w-10"
+				href="https://github.com/ucielsola"
+				target="_blank"
+				title="Uciel Sola's Github"
+			>
+				<GithubIcon />
+			</a>
+			<a
+				class="text-accent hover:text-primary h-10 w-10"
+				href="https://linkedin.com/in/ucielsola"
+				target="_blank"
+				title="Uciel Sola's Linkedin profile"
+			>
+				<LinkedInIcon />
+			</a>
+		</div>
+	</div>
 
 	<div class="grow overflow-hidden">
 		<MessageList />
