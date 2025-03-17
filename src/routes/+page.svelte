f3aa9f5852fdb90536765390fd39d697ecce5df7
Uciel Sola <solauciel@gmail.com>
2025-03-17 20:58:37 -0300

    decrease title size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 <div class="relative flex h-dvh w-dvw flex-col overflow-hidden bg-base-200 dark:bg-base-300">
 	<div class="flex items-center justify-between px-6 border-b border-secondary py-3">
 		<h1 class="absolute top-0 text-white/0">Uciel Sola</h1>
-		<h2 class="text-xl">UcielSola ~ Frontend Developer</h2>
+		<h2>UcielSola ~ Frontend Developer</h2>
 		<div class="flex items-center gap-3">
 			<a
 				class="btn btn-circle btn-accent dark:btn-primary p-1 text-white"
