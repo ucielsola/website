300ff184f065a8f9d5c036354434b6cdd81f2440
Uciel Sola <solauciel@gmail.com>
2025-03-17 21:00:56 -0300

    separated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,13 @@
 	<title>Uciel Sola</title>
 </svelte:head>
 
-<div class="relative flex h-dvh w-dvw flex-col overflow-hidden bg-base-200 dark:bg-base-300">
-	<div class="flex items-center justify-between px-6 border-b border-secondary py-3">
+<div class="bg-base-200 dark:bg-base-300 relative flex h-dvh w-dvw flex-col overflow-hidden">
+	<div class="border-secondary flex items-center justify-between border-b px-6 py-3">
 		<h1 class="absolute top-0 text-white/0">Uciel Sola</h1>
-		<h2>UcielSola ~ Frontend Developer</h2>
+		<div class="flex items-center gap-1">
+			<h2>UcielSola</h2>
+			<span class="text-sm">~ Frontend Developer</span>
+		</div>
 		<div class="flex items-center gap-3">
 			<a
 				class="btn btn-circle btn-accent dark:btn-primary p-1 text-white"
