e8d503544c8a9ff2fdca4715fd0318280a37728f
Uciel Sola <solauciel@gmail.com>
2025-04-08 23:48:33 -0300

    Refactor chat API to use environment variables for URL configuration

--- src/lib/api/chat.ts
+++ src/lib/api/chat.ts
@@ -1,24 +1,27 @@
-import type { Message } from '$lib/types';
-import { getTheDate } from '$lib/utils';
+import { PUBLIC_ENV, PUBLIC_LOCAL_CHAT_URL, PUBLIC_RAILWAY_CHAT_URL } from '$env/static/public';
+
 import { fetchEventSource, type EventSourceMessage } from '@microsoft/fetch-event-source';
+
+import { getTheDate } from '$lib/utils';
+
+import type { Message } from '$lib/types';
 
 export const sendMessages = async (
 	messages: Message[],
 	signal: AbortSignal,
 	onmessage: (e: EventSourceMessage) => void
 ) => {
-	const RAILWAY = 'https://ucielsola.up.railway.app/stream';
-	const LOCAL = 'http://localhost:8000/stream';
+	const URL = PUBLIC_ENV === 'local' ? PUBLIC_LOCAL_CHAT_URL : PUBLIC_RAILWAY_CHAT_URL;
 
 	const datems = getTheDate();
 
-	await fetchEventSource(LOCAL, {
+	await fetchEventSource(URL, {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({
-            messages: messages.filter((m) => m.content.length > 0),
-            datems
-        }),
+			messages: messages.filter((m) => m.content.length > 0),
+			datems
+		}),
 		onmessage,
 		signal
 	});
